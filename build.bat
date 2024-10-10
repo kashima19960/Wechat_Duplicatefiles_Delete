eef2ab79dd05b3adab0b90862fe8174b18ed2ea1
kashima19960 <2494389920@qq.com>
2024-10-10 23:11:06 +0800

    更新了用户界面和主程序，使用PyQt5库替换了tkinter，优化了文件删除工具的递归删除功能。

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple send2trash
 pip install pyinstaller
-pyinstaller --onefile main.py
+pyinstaller --onefile --windowed main.py
 pause
--- main.py
+++ main.py
@@ -1,9 +1,53 @@
 import os
 import stat
 import re
-import tkinter as tk
-from tkinter import filedialog
+from sys import argv
+from sys import exit
 import send2trash
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+from PyQt5 import QtCore, QtWidgets
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("文件删除工具")
+        Form.resize(383, 182)
+        self.verticalLayoutWidget = QtWidgets.QWidget(Form)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 381, 181))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.textBrowser = QtWidgets.QTextBrowser(self.verticalLayoutWidget)
+        self.textBrowser.setObjectName("textBrowser")
+        self.verticalLayout.addWidget(self.textBrowser, 0, QtCore.Qt.AlignVCenter)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.textBrowser.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu Mono derivative Powerline\',\'Consolas\',\'Courier New\',\'monospace\'; font-size:16pt; color:#0a3069;\">是否采取递归删除？</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:24px; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu Mono derivative Powerline\',\'Consolas\',\'Courier New\',\'monospace\'; font-size:16pt; color:#0a3069;\">注：递归删除的意思是，将文件子目录的文件也会一并删除</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:24px; background-color:#ffffff;\"><span style=\" font-family:\'Ubuntu Mono derivative Powerline\',\'Consolas\',\'Courier New\',\'monospace\'; font-size:16pt; color:#0a3069;\">请谨慎使用！！！</span></p></body></html>"))
+        self.pushButton.setText(_translate("Form", "是"))
+        self.pushButton_2.setText(_translate("Form", "否"))
+
 def delete_file(folder_path):
     pattern = re.compile(r'.*\(\d\).*')
     filelist=os.listdir(folder_path)
@@ -24,18 +68,26 @@ def delete_file(folder_path):
 def recursive_delete_file(directory):
     for root, dirs, files in os.walk(directory):
         delete_file(root)
-
-def main():
-    root = tk.Tk()
-    root.withdraw()  # 隐藏主窗口
-    print("是否采取递归删除？(y/n),递归操作相当危险，请谨慎选择")
-    flag=input()
-    file_path = filedialog.askdirectory() # 打开文件选择对话框
-    if flag=="y" or flag=='Y':
-        recursive_delete_file(file_path)
-    else:
-        delete_file(file_path)
-    root.destroy()
-    input("按回车键退出")
+class delete_tool_widget(QWidget,Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("文件删除工具")
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.yes_event)
+        self.pushButton_2.clicked.connect(self.no_event)
+    def yes_event(self):
+        directory = QFileDialog.getExistingDirectory(None,"选择目录",".",QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
+        recursive_delete_file(directory)
+        exit(app.exec_())
+    def no_event(self):
+        directory = QFileDialog.getExistingDirectory(None,"选择目录",".",QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
+        delete_file(directory)
+        exit(app.exec_())
+   
 if __name__=='__main__':
-    main()
+    app=QApplication(argv)
+    window=delete_tool_widget()
+    window.show()
+    exit(app.exec_())
+    input()
+    
